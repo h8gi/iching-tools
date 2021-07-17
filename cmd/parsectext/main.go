b3884fcb831fba2c52caae1eeefc279b01b04594
Hiroki Yagi <hiro.yagi0245@gmail.com>
2021-07-17 15:55:12 +0900

    add content struct

--- cmd/parsectext/main.go
+++ cmd/parsectext/main.go
@@ -5,19 +5,37 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+
+	"github.com/antchfx/htmlquery"
 )
 
 type gua struct {
 	id       string
 	filepath string
 	name     string
+	data     content
+}
+
+type content struct {
+	彖傳 string
+	象傳 string
 }
 
 func main() {
+	var guatable map[string]gua
+
 	files, _ := filepath.Glob("./files/*.html")
 	for _, fn := range files {
 		base := path.Base(fn)
 		id := strings.TrimSuffix(base, ".html")
 		fmt.Println(id)
 	}
+}
+
+func parseFile(filepath string) (content, error) {
+	doc, err := htmlquery.LoadDoc(filepath)
+	if err != nil {
+		return gua{}, err
+	}
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/h8gi/iching
 
 go 1.16
 
-require github.com/antchfx/htmlquery v1.2.3 // indirect
+require (
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	golang.org/x/tools/gopls v0.7.0 // indirect
+)
