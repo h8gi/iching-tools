38f218611489e76532ee1a99ac308561c16eb24b
Hiroki Yagi <hiro.yagi0245@gmail.com>
2021-07-22 13:13:14 +0900

    dl wikipedia

--- cmd/dlwikitext/main.go
+++ cmd/dlwikitext/main.go
@@ -1,7 +1,13 @@
 package main
 
+import "github.com/antchfx/htmlquery"
+
 const url = "https://en.wikipedia.org/wiki/List_of_hexagrams_of_the_I_Ching"
 
 func main() {
+	doc, err := htmlquery.LoadURL(url)
+	if err != nil {
+		panic(err)
+	}
 
 }
