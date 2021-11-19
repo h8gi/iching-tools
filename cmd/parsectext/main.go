d225bfc35cdc50fe610443b8e9bdc799e3337705
Hiroki Yagi <hiro.yagi0245@gmail.com>
2021-11-19 15:35:35 +0900

    parser

--- cmd/parsectext/main.go
+++ cmd/parsectext/main.go
@@ -17,25 +17,30 @@ type gua struct {
 }
 
 type content struct {
-	彖傳 string
-	象傳 string
+	TuanZhuan  string `json:"彖傳"`
+	XiangZhuan string `json:"象傳"`
 }
 
 func main() {
-	var guatable map[string]gua
+	// var guatable map[string]gua
 
 	files, _ := filepath.Glob("./files/*.html")
 	for _, fn := range files {
 		base := path.Base(fn)
 		id := strings.TrimSuffix(base, ".html")
 		fmt.Println(id)
+		parseFile(fn)
 	}
 }
 
-func parseFile(filepath string) (content, error) {
+func parseFile(filepath string) error {
 	doc, err := htmlquery.LoadDoc(filepath)
 	if err != nil {
-		return gua{}, err
+		return err
 	}
-
+	list := htmlquery.Find(doc, "//td[contains(@class, 'ctext')]")
+	for _, l := range list {
+		fmt.Println(htmlquery.InnerText(l))
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/h8gi/iching
 go 1.16
 
 require (
-	github.com/antchfx/htmlquery v1.2.3 // indirect
-	golang.org/x/tools/gopls v0.7.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
