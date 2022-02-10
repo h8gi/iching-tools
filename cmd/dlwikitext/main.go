e1ebb2a5c1dfea7961c15c34a942e63e2239a4da
yagi <hiro.yagi0245@gmail.com>
2022-02-10 09:30:05 +0900

    add files

--- cmd/dlwikitext/main.go
+++ cmd/dlwikitext/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/antchfx/htmlquery"
+import (
+	"fmt"
+
+	"github.com/antchfx/htmlquery"
+)
 
 const url = "https://en.wikipedia.org/wiki/List_of_hexagrams_of_the_I_Ching"
 
+const path = "./List_of_hexagrams_of_the_I_Ching.html"
+
 func main() {
-	doc, err := htmlquery.LoadURL(url)
+	doc, err := htmlquery.LoadDoc(path)
 	if err != nil {
 		panic(err)
 	}
-
+	fmt.Println(doc.FirstChild)
 }
